bf881ba841ced2ac38e8db2be854bf62495083c6
David <davidhillton@yahoo.com.ar>
2022-11-27 20:15:58 -0300

    cantidad de partidos

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,13 @@
-print('hola mundo')
+print("Bienvenido a la app de puntos de campeonato de 20 partidos")
+ganados  = int(input("Ingresar partidos ganados del eqipo: "))
+empatados = int(input("Ingresar partidos ganados del eqipo: "))
+perdidos = int(input("Ingresar partidos ganados del eqipo: "))
+
+cantidad = ganados + empatados + perdidos
+
+if cantidad == 20:
+    puntos = ganados * 3 + empatados * 1 
+    print("Los puntos del equipos son: ", puntos)
+
+else :
+    print("Cantidad de partidos incorrecto")
